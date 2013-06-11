2bf0a86fa69bd24ea145f0a40bf7ce962f3d1fbe
Katarzyna Kucharczyk <katarzyna.kucharczyk@cern.ch>
2013-06-11 17:44:00 +0200

    adding conf files template

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,3 @@
 class ganglia {
    include ganglia_init
-   asdf
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,15 +1,29 @@
 class ganglia::params {
-	
-   case $operatingsystem {
-      /(SLC|CerntOS)/: {
-         $conf_dir = '/etc/ganglia/'
-      }    
-   }
 
+  # configuration directories	
+  case $operatingsystem {
+    /(SLC|CerntOS)/: {
+      $conf_dir = '/etc/ganglia/'
+    }    
+  }
 
-   $node = 'gmond.conf'
-   $head = 'gmetad.conf'
-   $metics = 'metrics.conf'
+  $node = 'gmond.conf'
+  $head = 'gmetad.conf'
+  $metics = 'metrics.conf'
 
-
+  # params for gmond
+  # cluster
+  $cluster_name = 'OPENSTACK CLUSTER'
+  $owner = "CERN"
+  $latlong = "unknown"
+  $url = "unknown"  
+  
+  # connection
+  $location = "unknown"
+  $udp_recv_port = 8666
+  $mcast_join = 239.2.11.70
+  $udp_send_port = 8666
+  $ttl = 32
+  $mcast_join = 239.2.11.70
+  $tcp_accept_port = 8649 
 }
