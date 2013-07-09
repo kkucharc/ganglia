af4e585c17bf06b7fd71937f1701d3e2d9b3478e
Katarzyna Kucharczyk <kkucharc@kk-mchn-01.cern.ch>
2013-07-09 09:48:24 +0200

    check push

--- manifests/gmetad/install.pp
+++ manifests/gmetad/install.pp
@@ -4,4 +4,10 @@ class ganglia::install {
 		ensure => installed
    }
 
+   notify { 
+  'gmond-install':
+    message => 'Ganglia gmon package installed',
+    require => Package['ganglia-gmond']
+   }
+
 }
